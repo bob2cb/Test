1c2ac6b7a976bfcd1eb44ae666e8263125374f79
chenbo <chenbo@pxsj.com>
2017-11-05 13:32:16 +0800

    Add

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("1");
+        Debug.Log("2");
+        Debug.Log("3");
+        Debug.Log("4");
+        Debug.Log("5");
+        Debug.Log("6");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
