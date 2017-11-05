eb9adc7434e6aca9f6924c4562b36cca0b9d600a
chenbo <chenbo@pxsj.com>
2017-11-05 13:42:14 +0800

    Revert "回滚了"
    This reverts commit 1e2df17532c99e16c2ea9fcda3a8d69de73c2bdf.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,6 +9,9 @@ public class Test : MonoBehaviour {
         Debug.Log("2");
         Debug.Log("5");
         Debug.Log("6");
+        Debug.Log("7");
+        Debug.Log("8");
+        Debug.Log("9");
     }
 	
 	// Update is called once per frame
