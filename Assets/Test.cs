7214f4a4c1d2028329eed8a5a8547dcead9bd336
chenbo <chenbo@pxsj.com>
2017-11-05 13:46:46 +0800

    back

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,15 +5,7 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("1");
-        Debug.Log("2");
-        Debug.Log("3");
-        Debug.Log("4");
-        Debug.Log("5");
-        Debug.Log("6");
-        Debug.Log("7");
-        Debug.Log("8");
-        Debug.Log("9");
+
     }
 	
 	// Update is called once per frame
