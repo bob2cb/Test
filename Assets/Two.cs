bc9dcf459d3b82629f7ccd3cbca950928436facd
chenbo <chenbo@pxsj.com>
2017-11-05 13:50:28 +0800

    Add debug two 2

--- Assets/Two.cs
+++ Assets/Two.cs
@@ -6,6 +6,7 @@ public class Two : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Two 1");
+        Debug.Log("Two 2");
     }
 	
 	// Update is called once per frame
